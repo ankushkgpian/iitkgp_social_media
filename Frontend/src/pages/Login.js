20519305c90295a06003df43e652ecb499fb1f06
ankushkgpian <104966362+ankushkgpian@users.noreply.github.com>
2025-06-27 18:14:54 +0530

    Update Login.js

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+const API = process.env.REACT_APP_BACKEND_URL;
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -13,10 +14,11 @@ const Login = () => {
     setError('');
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
-        email,
-        password,
-      });
+      const res = await axios.post(`${API}/api/auth/login`, {
+      email,
+      password,
+    });
+
 
       // ✅ Save token and user
       localStorage.setItem('token', res.data.token);
