8643457c0c3e2e56cbdc030cd687b422716a5ca0
ankushkgpian <104966362+ankushkgpian@users.noreply.github.com>
2025-06-28 14:09:54 +0530

    Create Comment.js

---
+++ backend/models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  anonymous: {
+    type: Boolean,
+    default: false
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
