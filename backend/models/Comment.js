fd1845feb5407a694ea07978bac4e7fe0003712a
ankushkgpian <104966362+ankushkgpian@users.noreply.github.com>
2025-06-28 15:07:56 +0530

    Update Comment.js

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,35 +1,13 @@
+// models/Comment.js
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true
-  },
-  anonymous: {
-    type: Boolean,
-    default: false
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
-    required: true
-  },
-  upvotes: {
-  type: Number,
-  default: 0
-},
-upvotedBy: [{
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'User'
-}],
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+  content: String,
+  anonymous: Boolean,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+  upvotes: { type: Number, default: 0 },
+  upvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
 
 module.exports = mongoose.model('Comment', commentSchema);
